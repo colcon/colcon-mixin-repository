fb8ab8b488163ef7420846e827442d6292d03547
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-09-05 21:16:24 -0700

    add script to check the yaml style

---
+++ lint.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Lint the index.yaml file as well as all files ending with .mixin."""
+
+import os
+import sys
+
+try:
+    from yamllint.cli import run
+except ImportError:
+    sys.exit("Failed to import Python package 'yamllint'")
+
+any_error = False
+for name in sorted(os.listdir()):
+    if name != 'index.yaml' and not name.endswith('.mixin'):
+        continue
+
+    try:
+        run([
+            '--config-data',
+            '{'
+            'extends: default, '
+            'rules: {'
+            'document-start: {present: false}, '
+            'empty-lines: {max: 0}, '
+            'key-ordering: {}'
+            '}'
+            '}',
+            '--strict',
+            name,
+        ])
+    except SystemExit as e:
+        any_error |= bool(e.code)
+        continue
+    assert False, 'run() should always raise SystemExit'
+
+sys.exit(1 if any_error else 0)
